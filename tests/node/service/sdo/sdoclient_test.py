da8454c72e55dd6d248632325ad4b4b062d81d2b
Marcel Kanter <marcel.kanter@googlemail.com>
2022-05-07 10:43:54 +0200

    Skeleton for threaded tests
    Add skeletons for the helper test threads with the capability of failing the test.

--- tests/node/service/sdo/sdoclient_test.py
+++ tests/node/service/sdo/sdoclient_test.py
@@ -1,4 +1,6 @@
 import can
+import sys
+import threading
 import unittest
 
 from canopenx import Network, Node
@@ -8,14 +10,31 @@ from canopenx.node.service.sdo import SDOClient
 
 
 class SDOClientTestCase(unittest.TestCase):
+	def run(self, result = None):
+		# Save a reference to the result, the helper threads use them
+		if result == None:
+			self.result = self.defaultTestResult()
+		else:
+			self.result = result
+
+		unittest.TestCase.run(self, self.result)
+
+		return self.result
+
 	def test_expedited_download(self):
 		bus1 = can.ThreadSafeBus(interface = "virtual", channel = 0)
 		bus2 = can.ThreadSafeBus(interface = "virtual", channel = 0)
+		helper = threading.Thread(target = handle_expedited_download, args = (self, bus2), daemon = True)
+
 		network = Network()
 		network.connect(bus1)
 
 		node = ServiceTestNode()
 		network.add(node)
+
+		helper.start()
+
+		helper.join(1.0)
 
 		network.disconnect()
 		bus1.shutdown()
@@ -24,11 +43,17 @@ class SDOClientTestCase(unittest.TestCase):
 	def test_expedited_upload(self):
 		bus1 = can.ThreadSafeBus(interface = "virtual", channel = 0)
 		bus2 = can.ThreadSafeBus(interface = "virtual", channel = 0)
+		helper = threading.Thread(target = handle_expedited_upload, args = (self, bus2), daemon = True)
+
 		network = Network()
 		network.connect(bus1)
 
 		node = ServiceTestNode()
 		network.add(node)
+
+		helper.start()
+
+		helper.join(1.0)
 
 		network.disconnect()
 		bus1.shutdown()
@@ -37,11 +62,17 @@ class SDOClientTestCase(unittest.TestCase):
 	def test_segmented_download(self):
 		bus1 = can.ThreadSafeBus(interface = "virtual", channel = 0)
 		bus2 = can.ThreadSafeBus(interface = "virtual", channel = 0)
+		helper = threading.Thread(target = handle_segmented_download, args = (self, bus2), daemon = True)
+
 		network = Network()
 		network.connect(bus1)
 
 		node = ServiceTestNode()
 		network.add(node)
+
+		helper.start()
+
+		helper.join(1.0)
 
 		network.disconnect()
 		bus1.shutdown()
@@ -50,11 +81,17 @@ class SDOClientTestCase(unittest.TestCase):
 	def test_segmented_upload(self):
 		bus1 = can.ThreadSafeBus(interface = "virtual", channel = 0)
 		bus2 = can.ThreadSafeBus(interface = "virtual", channel = 0)
+		helper = threading.Thread(target = handle_segmented_upload, args = (self, bus2), daemon = True)
+
 		network = Network()
 		network.connect(bus1)
 
 		node = ServiceTestNode()
 		network.add(node)
+
+		helper.start()
+
+		helper.join(1.0)
 
 		network.disconnect()
 		bus1.shutdown()
@@ -76,3 +113,39 @@ class ServiceTestNode(Node):
 	def detach(self):
 		self.sdo.detach()
 		Node.detach(self)
+
+
+def handle_expedited_download(testcase, bus):
+	try:
+		pass
+	except AssertionError:
+		testcase.result.addFailure(testcase, sys.exc_info())
+	except:
+		testcase.result.addError(testcase, sys.exc_info())
+
+
+def handle_expedited_upload(testcase, bus):
+	try:
+		pass
+	except AssertionError:
+		testcase.result.addFailure(testcase, sys.exc_info())
+	except:
+		testcase.result.addError(testcase, sys.exc_info())
+
+
+def handle_segmented_download(testcase, bus):
+	try:
+		pass
+	except AssertionError:
+		testcase.result.addFailure(testcase, sys.exc_info())
+	except:
+		testcase.result.addError(testcase, sys.exc_info())
+
+
+def handle_segmented_upload(testcase, bus):
+	try:
+		pass
+	except AssertionError:
+		testcase.result.addFailure(testcase, sys.exc_info())
+	except:
+		testcase.result.addError(testcase, sys.exc_info())
